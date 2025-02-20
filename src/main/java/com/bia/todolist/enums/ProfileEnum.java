5f34e3ae49cfbe6bd9b18014fbfc380eee858ab7
Guilherme Alves <guilherme.alves1amo@gmail.com>
2025-02-20 10:02:49 -0300

    Feat: UserSecurity

--- src/main/java/com/bia/todolist/enums/ProfileEnum.java
+++ src/main/java/com/bia/todolist/enums/ProfileEnum.java
@@ -2,7 +2,6 @@ package com.bia.todolist.enums;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import java.util.Objects;
 
 @AllArgsConstructor
 @Getter
@@ -11,15 +10,16 @@ public enum  ProfileEnum {
     USER(2,"ROW_USER");
     private int code;
     private String description;
-    public static ProfileEnum toEnum(Integer code) {
-        if (Objects.isNull(code))
+
+    public ProfileEnum toEnum(Integer code){
+        if(code == null){
             return null;
-
-        for (ProfileEnum x : ProfileEnum.values()) {
-            if (code.equals(x.getCode()))
-                return x;
         }
-
-        throw new IllegalArgumentException("Invalid code: " + code);
+        for(ProfileEnum x : ProfileEnum.values()){
+            if(code.equals(x.getCode())){
+                return x;
+            }
+        }
+        throw new IllegalArgumentException("Invalid id: "+code);
     }
 }
